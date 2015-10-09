92ae51c46626d1c85a3e0af3863392d5cbc88ce8
costajob <costajob@gmail.com>
2015-10-09 17:25:28 +0200

    Mike - use delegation instead of subclassing for plain entity

--- lib/thron/entity/plain.rb
+++ lib/thron/entity/plain.rb
@@ -2,26 +2,29 @@ require 'ostruct'
 
 module Thron
   module Entity
-    class Plain < OpenStruct
+    class Plain
       class << self
         alias_method :factory, :new
-      end 
+      end
 
-      def initialize(hash = nil)
-        @hash_table = {}
-        if hash
-          hash.each do |k,v|
-            @hash_table[k.to_sym] = v
-            new_ostruct_member(k)
-          end
-        end
+      def initialize(hash = {})
+        @inner = OpenStruct::new(hash)
       end
 
       def to_h(*args)
-        @hash_table
+        @inner.to_h
+      end
+
+      def to_s
+        @inner.to_s
       end
 
       alias_method :to_payload, :to_h
+      alias_method :inspect, :to_s
+
+      def method_missing(name, *args, &block)
+        @inner.send(name, *args, &block)
+      end
     end
   end
 end
